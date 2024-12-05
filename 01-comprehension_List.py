2ddd40434183c7e4680aa292f2f85900ad93d89e
Roberto Bravo <robertbass@hotmail.com>
2024-12-05 11:04:06 -0500

    COMPREHENSION LIST

--- 01-comprehension_List.py
+++ 01-comprehension_List.py
@@ -1,15 +1,22 @@
 #COMPREHENSION LIST
 
+# CUADRADOS
 squares = [x**2 for x in range(1,11)]
 print(f'Cuadrados: {squares}')
 
+
+#CONVERSIONES
 celsius = [0, 10, 20, 40]
 fahrenheit = [(temp * 9/5) + 32 for temp in celsius]
 print(f'Celsius to Fahrenheit: {fahrenheit}')
 
+
+# NUMEROS PARES
 pairs = [x for x in range(1,21) if x%2==0]
 print(f'Pares: {pairs}')
 
+
+# MATRIZ Y SU TRANSPUESTA
 matrix = [
     [1,2,3],
     [4,5,6],
