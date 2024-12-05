36a133e5361df3699a0ed39ca7cb6ce7bd090c62
Roberto Bravo <robertbass@hotmail.com>
2024-12-05 10:42:35 -0500

    COMPREHENSION LIST

---
+++ 01-comprehension_List.py
@@ -0,0 +1,21 @@
+#COMPREHENSION LIST
+
+squares = [x**2 for x in range(1,11)]
+print(f'Cuadrados: {squares}')
+
+celsius = [0, 10, 20, 40]
+fahrenheit = [(temp * 9/5) + 32 for temp in celsius]
+print(f'Celsius to Fahrenheit: {fahrenheit}')
+
+pairs = [x for x in range(1,21) if x%2==0]
+print(f'Pares: {pairs}')
+
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+trans = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+print(matrix)
+print(trans)
